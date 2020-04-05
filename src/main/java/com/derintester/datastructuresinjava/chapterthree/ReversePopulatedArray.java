03ed70ac4f16e9e6cdcc1e11ed902192b3c50ccb
Derin Gbadebo <daireenonline@yahoo.com>
2020-04-04 17:28:55 -0700

    upgraded to log4j2

--- src/main/java/com/derintester/datastructuresinjava/chapterthree/ReversePopulatedArray.java
+++ src/main/java/com/derintester/datastructuresinjava/chapterthree/ReversePopulatedArray.java
@@ -2,8 +2,8 @@ package com.derintester.datastructuresinjava.chapterthree;
 
 import java.util.Arrays;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ReversePopulatedArray {
 	
